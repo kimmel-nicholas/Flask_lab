88c3b9c7974850336202e844f1883ea6174730ad
kimmel-nicholas <kimmel_nicholas@columbusstate.edu>
2021-09-19 23:16:58 +0000

    all

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/")                
+def hello():                    
+    return render_template("index.html") 
+
+@app.route("/about")
+def about():
+    name = request.args.get('name') if request.args.get('name') else "Hello World!" 
+    return render_template("about.html", aboutName=name)
+
+@app.route("/<name>")
+def hello_name(name):
+    return "Hello "+ name  
+
+@app.route("/")                
+def contact():                    
+    return render_template("contact.html") 
+
+if __name__ == "__main__":
+    app.run(debug=True)
